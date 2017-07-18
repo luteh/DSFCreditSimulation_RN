e915aad60760c540382ea1824e077d0ea67a696b
luteh <luthfanmaftuh@gmail.com>
2017-07-18 15:44:21 +0700

    Change fontSize

--- src/components/common/ResultText.js
+++ src/components/common/ResultText.js
@@ -9,7 +9,7 @@ import {Divider} from 'react-native-elements'
 const ResultText = ({titleText, detailText}) => {
     return (
         <View style={{marginBottom: 5}}>
-            <Text style={{fontSize: 10,}}>{titleText}</Text>
+            <Text style={{fontSize: 12,}}>{titleText}</Text>
             <Text style={{marginBottom: 5}}>{detailText}</Text>
             <Divider style={{backgroundColor:'#EEEEEE'}}/>
         </View>
